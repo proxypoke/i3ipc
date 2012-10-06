ea442446d593c620a3b2abb885072e96bd5676a0
Slo W. Poke <mail+git@slowpoke.io>
2012-10-06 22:54:29 +0000

    Adds Command() and Raw() functions + tests.

--- i3.go
+++ i3.go
@@ -2,10 +2,24 @@ package i3
 
 import (
 	"bytes"
+	"encoding/json"
 	"net"
 	"os/exec"
 	"strings"
 )
+
+// Struct for replies from command messages.
+type CommandReply struct {
+	Success bool
+	Error   string
+}
+
+// Error for replies from a command to i3.
+type CommandError string
+
+func (self CommandError) Error() string {
+	return string(self)
+}
 
 // Connect to the i3 IPC socket and return it.
 func GetIPCSocket() (ipc net.Conn, err error) {
@@ -20,5 +34,46 @@ func GetIPCSocket() (ipc net.Conn, err error) {
 
 	path := strings.TrimSpace(out.String())
 	ipc, err = net.Dial("unix", path)
+	return
+}
+
+// Send raw messages to i3. Returns a json bytestring.
+// FIXME: Uses exec to access i3-msg for now. Should use the socket instead.
+// FIXME: type_ should be an enumerated constant, not a string.
+func Raw(type_, args string, _ net.Conn) (json_reply []byte, err error) {
+	var out bytes.Buffer
+
+	cmd := exec.Command("i3-msg", "-t", type_, args)
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		return
+	}
+
+	json_reply = out.Bytes()
+	return
+}
+
+// Send a command to i3.
+// FIXME: Doesn't support chained commands yet.
+func Command(action string, ipc net.Conn) (success bool, err error) {
+	json_reply, err := Raw("command", action, ipc)
+	if err != nil {
+		return
+	}
+
+	var cmd_reply []CommandReply
+	err = json.Unmarshal(json_reply, &cmd_reply)
+	if err != nil {
+		return
+	}
+
+	success = cmd_reply[0].Success
+	if cmd_reply[0].Error == "" {
+		err = nil
+	} else {
+		err = CommandError(cmd_reply[0].Error)
+	}
+
 	return
 }
--- i3_test.go
+++ i3_test.go
@@ -4,22 +4,33 @@ import (
 	"testing"
 )
 
-
 func TestGetIPCSocket(t *testing.T) {
 	ipc, err := GetIPCSocket()
+	defer ipc.Close()
 	if err != nil {
-		t.Errorf("%v", err)
+		t.Errorf("Failed to acquire the IPC socket: %v", err)
 	}
-	_, err = ipc.Write([]byte("i3-ipc"))
-	if err != nil {
-		t.Errorf("Failed to write to the IPC socket: %v", err)
-	}
+}
 
-	reply := make([]byte, 1024)
-	_, err =ipc.Read(reply)
-	if err != nil {
-		t.Errorf("Failed to read from the IPC socket: %v", err)
-	}
+func TestRaw(t *testing.T) {
+	ipc, _ := GetIPCSocket()
 
-	ipc.Close()
+	_, err := Raw("get_workspaces", "", ipc)
+	if err != nil {
+		t.Errorf("Raw message sending failed: %v", err)
+	}
+}
+
+func TestCommand(t *testing.T) {
+	ipc, _ := GetIPCSocket()
+	defer ipc.Close()
+
+	// `exec /bin/true` is a good NOP operation for testing
+	success, err := Command("exec /bin/true", ipc)
+	if !success {
+		t.Error("Unsuccessful command.")
+	}
+	if err != nil {
+		t.Errorf("An error occurred during command: %v", err)
+	}
 }

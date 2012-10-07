e3872c816a18618a1712bba5aa9c68bf95a96412
Slo W. Poke <mail+git@slowpoke.io>
2012-10-07 00:12:45 +0000

    Adds MessageType constants for the various message types.
    Also changes all other code to use them.

--- i3ipc.go
+++ i3ipc.go
@@ -8,6 +8,26 @@ import (
 	"strings"
 )
 
+// The types of messages that Raw() accepts.
+type MessageType int
+
+const (
+	I3Command MessageType = iota
+	I3GetWorkspaces
+	I3GetOutputs
+	I3GetTree
+	I3GetMarks
+	I3GetBarConfig
+	I3GetVersion
+)
+
+// Error for unknown message types.
+type MessageTypeError string
+
+func (self MessageTypeError) Error() string {
+	return string(self)
+}
+
 // Struct for replies from command messages.
 type CommandReply struct {
 	Success bool
@@ -39,11 +59,33 @@ func GetIPCSocket() (ipc net.Conn, err error) {
 
 // Send raw messages to i3. Returns a json bytestring.
 // FIXME: Uses exec to access i3-msg for now. Should use the socket instead.
-// FIXME: type_ should be an enumerated constant, not a string.
-func Raw(type_, args string, _ net.Conn) (json_reply []byte, err error) {
-	var out bytes.Buffer
+func Raw(type_ MessageType, args string, _ net.Conn) (json_reply []byte, err error) {
+	var (
+		out        bytes.Buffer
+		typestring string
+	)
 
-	cmd := exec.Command("i3-msg", "-t", type_, args)
+	switch type_ {
+	case I3Command:
+		typestring = "command"
+	case I3GetWorkspaces:
+		typestring = "get_workspaces"
+	case I3GetOutputs:
+		typestring = "get_outputs"
+	case I3GetTree:
+		typestring = "get_tree"
+	case I3GetMarks:
+		typestring = "get_marks"
+	case I3GetBarConfig:
+		typestring = "get_bar_config"
+	case I3GetVersion:
+		typestring = "get_version"
+	default:
+		err = MessageTypeError("Unknown message type.")
+		return
+	}
+
+	cmd := exec.Command("i3-msg", "-t", typestring, args)
 	cmd.Stdout = &out
 	err = cmd.Run()
 	if err != nil {
@@ -57,7 +99,7 @@ func Raw(type_, args string, _ net.Conn) (json_reply []byte, err error) {
 // Send a command to i3.
 // FIXME: Doesn't support chained commands yet.
 func Command(action string, ipc net.Conn) (success bool, err error) {
-	json_reply, err := Raw("command", action, ipc)
+	json_reply, err := Raw(I3Command, action, ipc)
 	if err != nil {
 		return
 	}

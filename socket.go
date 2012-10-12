8ddaf9a6f0c65a36be48dc079e56ad7ee10b3dd4
Slo W. Poke <mail+git@slowpoke.io>
2012-10-12 14:02:07 +0000

    Replaces the hacky message receiving in Raw().
    The IPC socket now has a private recv() method, which handles reading
    from the socket in a rather elegant and, more importantly, correct way.

--- socket.go
+++ socket.go
@@ -2,6 +2,7 @@ package i3ipc
 
 import (
 	"bytes"
+	"fmt"
 	"net"
 	"os/exec"
 	"strings"
@@ -9,7 +10,13 @@ import (
 )
 
 // Magic string for the IPC API.
-const MAGIC string = "i3-ipc"
+const (
+	MAGIC string = "i3-ipc"
+	// The length of the i3 message "header" is 14 bytes: 6 for the MAGIC
+	// string, 4 for the length of the payload (int32 in native byte order) and
+	// another 4 for the message type (also int32 in NBO).
+	HEADERLEN = 14
+)
 
 // The types of messages that Raw() accepts.
 type MessageType int32
@@ -29,6 +36,13 @@ const (
 type MessageTypeError string
 
 func (self MessageTypeError) Error() string {
+	return string(self)
+}
+
+// Error for communiation failures.
+type MessageError string
+
+func (self MessageError) Error() string {
 	return string(self)
 }
 
@@ -63,6 +77,37 @@ func GetIPCSocket() (ipc IPCSocket, err error) {
 	return
 }
 
+// Receive a raw json bytestring from the socket.
+func (self IPCSocket) recv() (json_reply []byte, err error) {
+	header := make([]byte, HEADERLEN)
+	n, err := self.socket.Read(header)
+
+	// Check if this is a valid i3 message.
+	if n != HEADERLEN || err != nil {
+		return
+	}
+	magic_string := string(header[:len(MAGIC)])
+	if magic_string != MAGIC {
+		err = MessageError(fmt.Sprintf(
+			"Invalid magic string: got %q, expected %q.",
+			magic_string, MAGIC))
+		return
+	}
+
+	var bytelen [4]byte
+	// Copy the byte values from the slice into the byte array. This is
+	// necessary because the adress of a slice does not point to the actual
+	// values in memory.
+	for i, b := range header[len(MAGIC) : len(MAGIC)+4] {
+		bytelen[i] = b
+	}
+	length := *(*int32)(unsafe.Pointer(&bytelen))
+
+	payload := make([]byte, length)
+	n, err = self.socket.Read(payload)
+	return
+}
+
 // Send raw messages to i3. Returns a json bytestring.
 func (self IPCSocket) Raw(type_ MessageType, args string) (json_reply []byte, err error) {
 	// Set up the parts of the message.
@@ -93,24 +138,6 @@ func (self IPCSocket) Raw(type_ MessageType, args string) (json_reply []byte, er
 		return
 	}
 
-	// Receive and assemble the reply.
-	// Not sure if there's a cleaner solution but it seems to work.
-	for {
-		tmp := make([]byte, 1024)
-		n, err := self.socket.Read(tmp)
-
-		for _, b := range tmp {
-			json_reply = append(json_reply, b)
-		}
-		if n < 1024 || err != nil {
-			break
-		}
-	}
-
-	// Strip the first 14 bytes, which are the MAGIC string (6 bytes), the
-	// payload length (4 bytes) and the message type (another 4 bytes).
-	json_reply = json_reply[14:]
-	// Get rid of trailing null bytes.
-	json_reply = bytes.Trim(json_reply, "\x00")
+	json_reply, err = self.recv()
 	return
 }
